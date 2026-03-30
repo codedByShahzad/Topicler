00905fd5cc9188cce341ec6d751aad1c2f13bda2
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-03-30 18:17:06 +0500

    Made Some Changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     siteName: "Topicler",
     images: [
       {
-        url: "/images/ogImage.png",
+        url: "https://topickler.netlify.app/images/ogImage.png",
         width: 1200,
         height: 630,
         alt: "Topicler Blog Platform",

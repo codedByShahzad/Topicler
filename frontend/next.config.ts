96a75122206e60f36cdb8c8708a58706503262fc
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-04-01 12:55:50 +0500

    Added Some changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // 👈 THIS IS REQUIRED for Hostinger
+
   images: {
+    unoptimized: true, // 👈 REQUIRED for static export
     remotePatterns: [
       {
         protocol: "https",
